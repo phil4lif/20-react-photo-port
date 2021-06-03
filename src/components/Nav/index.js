ddca653e646f55a4448608ad6bbac73856ffa970
phil cowan <philipjcowan1@gmail.com>
2021-06-03 15:46:22 -0700

    most of about page

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,6 +1,25 @@
 import React from 'react';
 
 const Nav = () => {
+
+    const categories = [
+        {
+            name: "commercial",
+            description:
+                "Photos of grocery stores, food trucks, and other commercial projects",
+        },
+        { name: "portraits", description: "Portraits of people in my life" },
+        { name: "food", description: "Delicious delicacies" },
+        {
+            name: "landscape",
+            description: "Fields, farmhouses, waterfalls, and the beauty of nature",
+        },
+    ];
+
+    const categorySelected = (n) => {
+        console.log('hello category ' + n)
+    }
+
     return (
         <header>
             <h2>
@@ -18,6 +37,16 @@ const Nav = () => {
                     <li>
                         <span>Contact</span>
                     </li>
+                    {categories.map((category) => (
+                        <li
+                            className="mx-1"
+                            key={category.name}
+                        >
+                            <span onClick={() => categorySelected(category.name)}>
+                                {category.name}
+                            </span>
+                        </li>
+                    ))}
                 </ul>
             </nav>
         </header>
